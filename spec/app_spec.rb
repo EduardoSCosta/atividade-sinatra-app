6289cffe8c403a220d01f0455048457bc9138dca
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-02-03 09:09:22 -0300

    create test for when the route doesn't exist

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -15,16 +15,21 @@ describe 'App' do
 
   it "return response 200 from the / route" do
     get '/'
-    expect(last_response).to be_ok
+    expect(last_response.status).to eq(200)
   end
 
   it "return response 200 from the /myprs route" do
     get '/my_prs'
-    expect(last_response).to be_ok
+    expect(last_response.status).to eq(200)
   end
 
   it "return response 200 from the /finalpaper route" do
     get '/final_paper'
-    expect(last_response).to be_ok
+    expect(last_response.status).to eq(200)
+  end
+
+  it "return response 404 when the route doesn't exist" do
+    get '/doesnt_exist'
+    expect(last_response.status).to eq(404)
   end
 end
