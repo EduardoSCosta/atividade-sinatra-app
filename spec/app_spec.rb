77f2f59d1af469571152e9cf2593df4bf061cd7a
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-02-07 12:30:32 -0300

    include context in the tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -14,30 +14,38 @@ describe 'App' do
   end
 
   describe 'get /' do
-    it "return response 200 from the / route" do
-      get '/'
-      expect(last_response.status).to eq(200)
+    context 'when the route exist' do
+      it "return response 200 from the / route" do
+        get '/'
+        expect(last_response.status).to eq(200)
+      end
     end
   end
 
   describe 'get /my_prs' do
-    it "return response 200 from the /myprs route" do
-      get '/my_prs'
-      expect(last_response.status).to eq(200)
+    context 'when the route exist' do
+      it "return response 200 from the /myprs route" do
+        get '/my_prs'
+        expect(last_response.status).to eq(200)
+      end
     end
   end
 
   describe 'get /final_paper' do
-    it "return response 200 from the /finalpaper route" do
-      get '/final_paper'
-      expect(last_response.status).to eq(200)
+    context 'when the route exist' do
+      it "return response 200 from the /finalpaper route" do
+        get '/final_paper'
+        expect(last_response.status).to eq(200)
+      end
     end
   end
 
   describe 'get nonexistent route' do
-    it "return response 404 when the route doesn't exist" do
-      get '/doesnt_exist'
-      expect(last_response.status).to eq(404)
+    context "when the route doesn't exist" do
+      it "return response 404" do
+        get '/doesnt_exist'
+        expect(last_response.status).to eq(404)
+      end
     end
   end
 end
